b569dfca0805db403f8c2fa924f3d2ab0d6e095f
chenchen1415 <chenchen@wuxicloud.com>
2020-07-20 14:17:09 +0800

    feat：eslint规则、webpack相关配置

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,21 +1,28 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <!-- <img alt="Vue logo" src="../assets/logo.png" />
+    <HelloWorld msg="Welcome to Your Vue.js App" /> -->
+    <div>env: {{ env }}</div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+// import HelloWorld from "@/components/HelloWorld.vue";
 
 export default {
-  name: "Home",
+  name: 'Home',
   components: {
-    HelloWorld
+    // HelloWorld
+  },
+  data() {
+    return {
+      env: ''
+    }
   },
   mounted() {
-    console.log("env:", process.env);
+    this.env = process.env
+    console.log('env:', process.env)
   }
-};
+}
 </script>
