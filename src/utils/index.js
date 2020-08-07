d49e82ee088a952c0de2d0c9061766c0368d056d
chenchen1415 <chenchen@wuxicloud.com>
2020-08-07 12:13:16 +0800

    feat：新增importAll方法，引入当前某路径下的所有文件

--- src/utils/index.js
+++ src/utils/index.js
@@ -8,3 +8,26 @@ export const clearLs = () => localStorage.clear();
 
 // 根据后端返回的权限路由，过滤出本地的路由集合
 // 原因：component无法进行保存
+export const filterRoutes = (AllRoutes, permissionRoutes) => {
+  let afterFilterRoutes = [];
+  AllRoutes.forEach(ai => {
+    permissionRoutes.forEach(pi => {
+      if (ai.name === pi.name) {
+        if (ai.children && ai.children.length > 0) {
+          ai.children = filterRoutes(ai.children, pi.children);
+        }
+        afterFilterRoutes.push(ai);
+      }
+    });
+  });
+  return afterFilterRoutes;
+};
+
+// 导入路径下所有模块
+export const importAll = context => {
+  const map = {}
+  context.keys().forEach(file => {
+    map[file] = context(file)
+  })
+  return map
+}
