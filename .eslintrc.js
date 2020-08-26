bfaff91635744d74a867a0609cd8b1e540d60703
chenchen1415 <chenchen@wuxicloud.com>
2020-08-26 17:53:59 +0800

    feat：下拉选滚动加载指令（待优化）

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,13 +1,13 @@
-// module.exports = {
-//   root: true,
-//   parserOptions: {
-//     parser: 'babel-eslint',
-//     sourceType: 'module'
-//   },
-//   env: {
-//     browser: true,
-//     node: true,
-//     es6: true
-//   },
-//   // extends: ['plugin:vue/recommended', 'eslint:recommended'],
-// }
+module.exports = {
+  root: true,
+  parserOptions: {
+    parser: 'babel-eslint',
+    sourceType: 'module'
+  },
+  env: {
+    browser: true,
+    node: true,
+    es6: true
+  },
+  extends: ['plugin:vue/recommended', 'eslint:recommended'],
+}
